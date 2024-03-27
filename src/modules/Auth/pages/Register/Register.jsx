19d5251c661da5a3d2371fcf99eee48bd2581e26
iballidev <iballi.dev@gmail.com>
2024-03-27 20:07:51 +0100

    update

--- src/modules/Auth/pages/Register/Register.jsx
+++ src/modules/Auth/pages/Register/Register.jsx
@@ -87,7 +87,7 @@ const Register = () => {
                   type="submit"
                   className="text-white bg-primary-500 hover:bg-primary-600 focus:ring-1 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 sm:py-3 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 >
-                  <span className="text-base sm:text-lg">Log In</span>
+                  <span className="text-base sm:text-lg">Create An Account</span>
                 </button>
               </form>
             </div>
