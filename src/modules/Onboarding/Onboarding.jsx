c8a7c34a547b780ae20522fe9e1a44d0feb1f70e
iballi2017 <iballi201@gmail.com>
2024-03-28 17:42:21 +0100

    update

--- src/modules/Onboarding/Onboarding.jsx
+++ src/modules/Onboarding/Onboarding.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import OnboardingToolbar from './components/OnboardingToolbar/OnboardingToolbar';
 import { Outlet } from 'react-router-dom';
 import OnboardingProgressBar from './components/OnboardingProgressBar/OnboardingProgressBar';
+import MobileAppBar from '../../shared/MobileAppBar/MobileAppBar';
 
 const Onboarding = () => {
     return (
@@ -12,6 +13,7 @@ const Onboarding = () => {
             <OnboardingProgressBar />
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <Outlet />
+                <MobileAppBar />
             </div>
         </>
     );
--- src/modules/Onboarding/pages/ConfigureBeats/QRGenerator/QRGenerator.jsx
+++ src/modules/Onboarding/pages/ConfigureBeats/QRGenerator/QRGenerator.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import QRCode from 'qrcode.react';
-import "./QRGenerator.scss"
+import React from "react";
+import QRCode from "qrcode.react";
+import "./QRGenerator.scss";
 
-const QRGenerator = ({ text }) => {
-    // console.log(<QRCode/>)
-    return (
-        <>
-            <div className="qrcode-container">
-                {{ text } && <QRCode value={text} size={220} fgColor="#008080"
-                    style={{ color: "red", margin: "0 auto" }} />}
-            </div>
-            {/* {console.log(<QRCode/>)} */}
-        </>
-    );
-}
+const QRGenerator = ({ text, qr_code_size }) => {
+  // console.log(<QRCode/>)
+  return (
+    <>
+      <div className="qrcode-container">
+        {{ text } && (
+          <QRCode
+            value={text}
+            size={qr_code_size}
+            fgColor="#008080"
+            style={{ color: "red", margin: "0 auto" }}
+          />
+        )}
+      </div>
+      {/* {console.log(<QRCode/>)} */}
+    </>
+  );
+};
 
 export default QRGenerator;
