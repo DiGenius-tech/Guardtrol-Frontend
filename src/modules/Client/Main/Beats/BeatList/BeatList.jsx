0c172e93f563534529815b860ef6bcec8f2e4f2a
iballidev <iballi.dev@gmail.com>
2024-04-16 13:20:01 +0100

    update

--- src/modules/Client/Main/Beats/BeatList/BeatList.jsx
+++ src/modules/Client/Main/Beats/BeatList/BeatList.jsx
@@ -11,12 +11,9 @@ const BeatList = () => {
         <>
             {/* beat-list-app works! */}
 
-            <div className="flex mb-4">
-                {/* <Link to={"configure-beat"} className="ml-auto border bg-primary-500 p-2 text-sm rounded-lg text-white shadow-md">
-                    Configure beat
-                </Link> */}
-                <Link to={"/onboarding/configure-beats/"} className="ml-auto border bg-primary-500 p-2 text-sm rounded-lg text-white shadow-md">
-                    Configure beat
+            <div className="fixed z-10 bottom-8 right-4">
+                <Link to={"/onboarding/configure-beats/"} className="ml-auto border bg-secondary-500 h-12 w-12 p-2 flex items-center justify-center text-lg font-bold rounded-full text-white shadow-md">
+                    +
                 </Link>
             </div>
             <div className="hidden sm:block">
--- src/modules/Client/Main/Settings/Settings.jsx
+++ src/modules/Client/Main/Settings/Settings.jsx
@@ -1,7 +1,29 @@
+import { Tabs } from "flowbite-react";
+
 function Settings() {
     return (  
         <>
-        settings-app works!
+        {/* settings-app works! */}
+
+
+
+        
+      <div className="tab flex-tabs flex-tab-nowrap">
+        <Tabs aria-label="Tabs with underline" style="underline">
+          <Tabs.Item active title="Personal information">
+            Personal information app
+          </Tabs.Item>
+          <Tabs.Item title="Security">
+            Security app
+          </Tabs.Item>
+          <Tabs.Item title="Shift schedule">
+            Shift schedule app
+          </Tabs.Item>
+          <Tabs.Item title="Billing">
+            Billing app
+          </Tabs.Item>
+        </Tabs>
+      </div>
         </>
     );
 }
