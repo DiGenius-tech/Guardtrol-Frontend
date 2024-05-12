323506b7cc385e8ef180744b3a2bfdc893e75c61
Victory <onorhimevictory2017@gmail.com>
2024-05-12 11:45:58 -0400

    new commit

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
-  process.env.NODE_ENV === "development"
+  process.env.REACT_APP_NODE_ENV === "development"
     ? "http://localhost:5000/api/"
-    : process.env.VITE_APP_API_URL;
+    : `${process.env.REACT_APP_VITE_APP_API_URL}api/`; 
 
 export const ASSET_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/"
-    : process.env.VITE_APP_API_URL;
+    : process.env.REACT_APP_VITE_APP_API_URL;
