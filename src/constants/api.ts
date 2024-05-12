b01e16651539d55b805276d4439c7bf331a06cd3
Muhammad Giwa <mgiwa78@gmail.com>
2024-05-12 13:16:52 +0100

    Update

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api/"
-    : process.env.VITE_APP_API_URL;
+    : process.env.LIVE_APP_API_URL;
 
 export const ASSET_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/"
-    : process.env.VITE_APP_API_URL;
+    : process.env.LIVE_APP_API_URL;
